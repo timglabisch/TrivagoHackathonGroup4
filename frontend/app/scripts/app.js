93260a3189d3f1eb560e71564d461e6552c874cc
Igor Besel <igor.besel@yahoo.com>
2014-07-05 14:38:01 +0200

    Karte

--- frontend/app/scripts/app.js
+++ frontend/app/scripts/app.js
@@ -9,7 +9,7 @@
  * Main module of the application.
  */
 angular
-  .module('frontendApp', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ngRoute'])
+  .module('frontendApp', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'ngRoute', 'google-maps'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
@@ -31,4 +31,52 @@ angular
       .otherwise({
         redirectTo: '/'
       });
+  })
+
+  .directive('tabs', function() {
+    return {
+      restrict: 'E',
+      transclude: true,
+      scope: {},
+      controller: function($scope, $element) {
+        var panes = $scope.panes = [];
+         
+        $scope.select = function(pane) {
+          angular.forEach(panes, function(pane) {
+            pane.selected = false;
+          });
+          pane.selected = true;
+        }
+         
+        this.addPane = function(pane) {
+          if (panes.length == 0) $scope.select(pane);
+          panes.push(pane);
+        }
+      },
+      template:
+        '<div class="tabbable">' +
+          '<div class="btn-group btn-group-justified">' +
+            '<div class="btn-group" ng-repeat="pane in panes" ng-class="{active:pane.selected}">' +
+              '<button type="button" class="btn btn-default" ng-click="select(pane)">{{pane.title}}</button>' +
+            '</div>' +
+          '</div>' +
+          '<div class="tab-content" ng-transclude></div>' +
+        '</div>',
+      replace: true
+    };
+  }) 
+  .directive('pane', function() {
+    return {
+      require: '^tabs',
+      restrict: 'E',
+      transclude: true,
+      scope: { title: '@' },
+      link: function(scope, element, attrs, tabsCtrl) {
+        tabsCtrl.addPane(scope);
+      },
+      template:
+        '<div class="tab-pane" ng-class="{active: selected}" ng-transclude>' +
+        '</div>',
+      replace: true
+    };
   });
--- frontend/app/scripts/controllers/offer.js
+++ frontend/app/scripts/controllers/offer.js
@@ -7,5 +7,11 @@
  */
 angular.module('frontendApp')
     .controller('OfferCtrl', function ($scope) {
-
+        $scope.map = {
+            center: {
+                latitude: 45,
+                longitude: -73
+            },
+            zoom: 8
+        };
     });
