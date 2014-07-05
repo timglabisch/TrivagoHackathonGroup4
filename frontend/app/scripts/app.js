fd58a34bd07e694e63e56a1f47760da03ae9dfa1
Rouven Weßling <me@rouvenwessling.de>
2014-07-05 11:43:24 +0200

    Yoeman template für das frontend

---
+++ frontend/app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name frontendApp
+ * @description
+ * # frontendApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('frontendApp', [
+    'ngAnimate',
+    'ngSanitize'
+  ]);
