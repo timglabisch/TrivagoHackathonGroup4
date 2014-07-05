3f105c5fcbdf10c3161e92400e924084e5cc0877
Rouven Weßling <me@rouvenwessling.de>
2014-07-05 20:24:57 +0200

    Maps für Hotelier App

--- hotelier/app/scripts/app.js
+++ hotelier/app/scripts/app.js
@@ -16,11 +16,46 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/app', {
+      .when('/:placeid', {
         templateUrl: 'views/app.html',
         controller: 'AppCtrl'
       })
       .otherwise({
         redirectTo: '/'
       });
-  });
+  })
+    .run(function ($rootScope, $http) {
+        $rootScope.hotels = [];
+
+        $http.get('/hotel.json')
+            .success(function(data, status, headers, config) {
+                $rootScope.hotels = data.data;
+            });
+    })
+    .factory('socket', function ($rootScope) {
+        var socket = io.connect('192.168.55.44:3001');
+
+        return {
+            emit: function (event, data, callback) {
+                socket.emit(event, JSON.stringify(data), function () {
+                    var args = arguments;
+                    $rootScope.$apply(function () {
+                        if (callback) {
+                            callback.apply(null, args);
+                        }
+                    });
+                });
+            },
+            on: function (event, callback) {
+                socket.on(event, function () {
+                    var args = arguments;
+                    $rootScope.$apply(function () {
+                        callback.apply(null, args);
+                    });
+                });
+            },
+            off: function (event, callback) {
+                socket.removeListener(event, callback);
+            }
+        };
+    });
